8588111247e88077e3e3ed9d504686ebc9da6a01
marcelodinamo <marcelolsreis@gmail.com>
2021-12-16 19:12:45 +0000

    16/12/2021

--- cpf.py
+++ cpf.py
@@ -1,22 +1,30 @@
 import os
 os.system('clear')
-
-cpf = [1,6,8,9,9,5,3,5,0]
-cont = 10
-soma = 0
-for i in cpf:
-    soma = (i * cont) + soma
-    cont -= 1
-digito1 = (11 - (soma % 11))
-if digito1 > 9:
-    digito1 = 0
-cpf.append(digito1)   
-cont = 11
-soma = 0
-for i in cpf:
-    soma = (i * cont) + soma 
-    cont -= 1
-digito2 = 11 -(soma % 11)
-cpf.append(digito2)
-cpf = str(cpf)
-print(cpf)
+cpfe = input('Entre com seu CPF: ')
+cpf = cpfe[:-2]  # escrui os dois ultimos digitos
+if cpf.isdecimal():  # verifica se o que foi digitado é decimal 
+    cpf = list(map(int, cpf)) # converte lista de string em lista de inteiros
+    cont = 10
+    soma = 0
+    for i in cpf:
+        soma = (i * cont) + soma
+        cont -= 1
+    digito1 = (11 - (soma % 11))
+    if digito1 > 9:
+        digito1 = 0
+    cpf.append(digito1)    
+    cont = 11
+    soma = 0
+    for i in cpf:
+        soma = (i * cont) + soma 
+        cont -= 1
+    digito2 = 11 -(soma % 11)
+    cpf.append(digito2)
+    cpfg = "".join(map(str, cpf))
+    print(cpfg)
+    if cpfg == cpfe:
+        print('CPF Valido!!')
+    else:
+        print('CPF invalido!!')    
+else:
+    print('Você digitou algo errado!!')     
