d747452ed5cc0b05ec833522e85a596e14765019
marcelodinamo <marcelolsreis@gmail.com>
2022-01-04 09:20:58 +0000

    04/01/2022

---
+++ numero_positivo.py
@@ -0,0 +1,16 @@
+# import os
+# os.system('clear')
+
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+e = float(input())
+f = float(input())
+lista = [a,b,c,d,e,f]
+contador = 0
+for i in lista:
+    if i > 0:
+        contador += 1 
+
+print(contador)
