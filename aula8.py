696783eb94e845ebba99811f0d7c6a55a6f6debd
marcelodinamo <marcelolsreis@gmail.com>
2022-01-18 00:29:12 +0000

    17/01/2022

--- aula8.py
+++ aula8.py
@@ -1,2 +1,14 @@
 import os
 os.system('clear')
+
+respostas = {
+    'items' : {
+    'item 1' : {'a' : '1', 'b' : '2', 'c' : '3'}
+}
+}
+
+for i, n in respostas.items():
+    for h, j in n['item 1'].items():
+        print(f'({h}) : {j}')
+
+
